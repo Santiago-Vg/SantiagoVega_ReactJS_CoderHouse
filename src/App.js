590107d53dbcb30e5b140e47922beebb691b2675
SantiagoVega <santi.lcdl@gmail.com>
2021-06-08 11:26:32 -0300

    Primer Commit del Proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tienda Online de CruxStudio</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Tempora dicta pariatur totam ipsa, laboriosam minus commodi 
+        adipisci perferendis fugiat animi a soluta, aperiam, mollitia 
+        autem eaque repellendus rem cupiditate laudantium!
+      </p>
     </div>
   );
 }
